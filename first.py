36b2e597c8b20e9c38469b1f05b8e243f160f471
shakeer <shakeer.uid@gmail.com>
2018-09-29 07:05:24 +0530

    seven

--- first.py
+++ first.py
@@ -3,4 +3,5 @@ print ("This is second edit of the program")
 print ("This is third edit of the program")
 print ("This is fourth edit of the program")
 print ("This is fifth edit of the program")
+print ("This is seven edit of the program")
 
